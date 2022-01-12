dc97557b066a24f64afadfca5c2517ad934a4eda
nikdimitriadis <dimitriadisnikos3@gmail.com>
2022-01-12 14:15:05 +0200

    fix_net_rem_Try_catch

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -4,7 +4,7 @@ import React, { useState, useCallback, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ColumnFlex from "../components/UI/LayOut/Flexes/ColumnFlex/ColumnFlex";
 import styles from "./Details.module.css";
-import NotFound from "./NotFound";
+
 // import Layout from "../components/UI/LayOut/Layout";
 
 const Details = () => {
@@ -64,127 +64,124 @@ const Details = () => {
     if (dataFromApi.platform.startsWith("Web")) {
       platInf = false;
     }
-    try {
-      content = (
-        <article className={styles.detailArt}>
-          {dataFromApi.screenshots.length > 0 && (
-            <DetailBg sreenOne={dataFromApi.screenshots[0].image} />
-          )}
-          <PCard>
-            <ColumnFlex>
-              <h1>{dataFromApi.title}</h1>
-              <div className={styles.doubleFlex}>
-                <div className={styles.firstInFlex}>
-                  <img src={dataFromApi.thumbnail} alt={dataFromApi.title} />
-                  <h3>Platform {dataFromApi.platform}</h3>
-                  <div className={styles["genre_details"]}>
-                    {dataFromApi.genre}
-                  </div>
-                  <div>
-                    <a
-                      href={dataFromApi.freetogame_profile_url}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      PLAY NOW
-                    </a>
-                  </div>
+    // try {
+    content = (
+      <article className={styles.detailArt}>
+        {dataFromApi.screenshots.length > 0 && (
+          <DetailBg sreenOne={dataFromApi.screenshots[0].image} />
+        )}
+        <PCard>
+          <ColumnFlex>
+            <h1>{dataFromApi.title}</h1>
+            <div className={styles.doubleFlex}>
+              <div className={styles.firstInFlex}>
+                <img src={dataFromApi.thumbnail} alt={dataFromApi.title} />
+                <h3>Platform {dataFromApi.platform}</h3>
+                <div className={styles["genre_details"]}>
+                  {dataFromApi.genre}
                 </div>
-                <div className={styles.aboutContent}>
-                  <h3>About</h3>
-                  <p>{dataFromApi.description}</p>
+                <div>
+                  <a
+                    href={dataFromApi.freetogame_profile_url}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    PLAY NOW
+                  </a>
                 </div>
               </div>
-
-              <div className={styles.dbleImg}>
-                {dataFromApi.screenshots[1] && (
-                  <img
-                    src={dataFromApi.screenshots[1].image}
-                    alt="live game action"
-                  />
-                )}
-                {dataFromApi.screenshots[2] && (
-                  <img
-                    src={dataFromApi.screenshots[2].image}
-                    alt="single player"
-                  />
-                )}
+              <div className={styles.aboutContent}>
+                <h3>About</h3>
+                <p>{dataFromApi.description}</p>
               </div>
+            </div>
 
-              <footer className={styles.footer}>
-                <div className={styles.footerFirstDiv}>
-                  <h3>Additional Information</h3>
-                  <p>{dataFromApi.short_description}</p>
-                  <h4>
-                    Developer <span>{dataFromApi.developer}</span>
-                  </h4>
-                  <h4>
-                    Publisher <span>{dataFromApi.publisher}</span>
-                  </h4>
-                  <h4>
-                    Release Date <span>{dataFromApi.release_date}</span>
-                  </h4>
-                </div>
-                {platInf && (
-                  <div className={styles.footerSecDiv}>
-                    <h3>
-                      Minimum System Requirements <br /> {dataFromApi.platform}
-                    </h3>
-                    <div className={styles.dbleImg}>
-                      <div className={styles.firstList}>
-                        <div className={styles.minDiv}>
-                          <h4>OS</h4>
-                          <p> {dataFromApi.minimum_system_requirements.os}</p>
-                        </div>
-                        <div className={styles.minDiv}>
-                          <h4>memory</h4>
-                          <p>
-                            {" "}
-                            {dataFromApi.minimum_system_requirements.memory}
-                          </p>
-                        </div>
-                        <div className={styles.minDiv}>
-                          <h4>Storage</h4>
-                          <p>
-                            {" "}
-                            {dataFromApi.minimum_system_requirements.storage}
-                          </p>
-                        </div>
+            <div className={styles.dbleImg}>
+              {dataFromApi.screenshots[1] && (
+                <img
+                  src={dataFromApi.screenshots[1].image}
+                  alt="live game action"
+                />
+              )}
+              {dataFromApi.screenshots[2] && (
+                <img
+                  src={dataFromApi.screenshots[2].image}
+                  alt="single player"
+                />
+              )}
+            </div>
+
+            <footer className={styles.footer}>
+              <div className={styles.footerFirstDiv}>
+                <h3>Additional Information</h3>
+                <p>{dataFromApi.short_description}</p>
+                <h4>
+                  Developer <span>{dataFromApi.developer}</span>
+                </h4>
+                <h4>
+                  Publisher <span>{dataFromApi.publisher}</span>
+                </h4>
+                <h4>
+                  Release Date <span>{dataFromApi.release_date}</span>
+                </h4>
+              </div>
+              {platInf && (
+                <div className={styles.footerSecDiv}>
+                  <h3>
+                    Minimum System Requirements <br /> {dataFromApi.platform}
+                  </h3>
+                  <div className={styles.dbleImg}>
+                    <div className={styles.firstList}>
+                      <div className={styles.minDiv}>
+                        <h4>OS</h4>
+                        <p> {dataFromApi.minimum_system_requirements.os}</p>
                       </div>
-                      <div className={styles.secondList}>
-                        <div className={styles.minDiv}>
-                          <h4>Processor</h4>
-                          <p>
-                            {dataFromApi.minimum_system_requirements.processor}
-                          </p>
-                        </div>
-                        <div className={styles.minDiv}>
-                          <h4>Graphics</h4>
-                          <p>
-                            {" "}
-                            {dataFromApi.minimum_system_requirements.graphics}
-                          </p>
-                        </div>
-                        <div className={styles.minDiv}>
-                          <h4>Additional Notes</h4>
-                          <p> Specifications may change during development</p>
-                        </div>
+                      <div className={styles.minDiv}>
+                        <h4>memory</h4>
+                        <p> {dataFromApi.minimum_system_requirements.memory}</p>
+                      </div>
+                      <div className={styles.minDiv}>
+                        <h4>Storage</h4>
+                        <p>
+                          {" "}
+                          {dataFromApi.minimum_system_requirements.storage}
+                        </p>
+                      </div>
+                    </div>
+                    <div className={styles.secondList}>
+                      <div className={styles.minDiv}>
+                        <h4>Processor</h4>
+                        <p>
+                          {dataFromApi.minimum_system_requirements.processor}
+                        </p>
+                      </div>
+                      <div className={styles.minDiv}>
+                        <h4>Graphics</h4>
+                        <p>
+                          {" "}
+                          {dataFromApi.minimum_system_requirements.graphics}
+                        </p>
+                      </div>
+                      <div className={styles.minDiv}>
+                        <h4>Additional Notes</h4>
+                        <p> Specifications may change during development</p>
                       </div>
                     </div>
                   </div>
-                )}
-              </footer>
-            </ColumnFlex>
-          </PCard>
-        </article>
-      );
+                </div>
+              )}
+            </footer>
+          </ColumnFlex>
+        </PCard>
+      </article>
+    );
 
-      if (dataFromApi.screenshots == undefined) {
-        throw new Error();
-      }
-    } catch (e) {
-      console.log(e);
-    }
+    //   if (dataFromApi.screenshots === undefined) {
+    //     throw new Error();
+    //   }
+    // } catch (e) {
+    //   console.log(e);
+    // }
   }
 
   useEffect(() => {
