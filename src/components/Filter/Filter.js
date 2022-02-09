5bca285719734446a4be0250024994557c40caee
nikdimitriadis <dimitriadisnikos3@gmail.com>
2022-02-09 22:08:48 +0200

    deleting comms

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -131,19 +131,20 @@ const Filter = (props) => {
           <div>
             <label htmlFor="platform">Choose Platform:</label>
             <select
+              value={isFiltering}
               onChange={(e) => platflorHandler(e.target.value)}
               name="platform"
               id="platform"
             >
               <option value="">Choose Platform</option>
               <option
-                selected={isFiltering === "platform=browser"}
+                // selected={isFiltering === "platform=browser"}
                 value="platform=browser"
               >
                 Web Broweser
               </option>
               <option
-                selected={isFiltering === "platform=pc"}
+                // selected={isFiltering === "platform=pc"}
                 value="platform=pc"
               >
                 PC (Windows)
@@ -155,65 +156,21 @@ const Filter = (props) => {
           <div>
             <label htmlFor="category">Choose Category:</label>
             <select
+              value={isFilteringSecond}
               onChange={(e) => catHandler(e.target.value)}
               name="category"
               id="category"
             >
               <option value="">Choose Category</option>
-              <option
-                selected={isFilteringSecond === "category=MMO"}
-                value="category=MMO"
-              >
-                MMO
-              </option>
-              <option
-                selected={isFilteringSecond === "category=MMORPG"}
-                value="category=MMORPG"
-              >
-                MMORPG
-              </option>
-              <option
-                selected={isFilteringSecond === "category=MOBA"}
-                value="category=MOBA"
-              >
-                MOBA
-              </option>
-              <option
-                selected={isFilteringSecond === "category=Shooter"}
-                value="category=Shooter"
-              >
-                Shooter
-              </option>
-              <option
-                selected={isFilteringSecond === "category=Strategy"}
-                value="category=Strategy"
-              >
-                Strategy
-              </option>
-              <option
-                selected={isFilteringSecond === "category=Racing"}
-                value="category=Racing"
-              >
-                Racing
-              </option>
-              <option
-                selected={isFilteringSecond === "category=Social"}
-                value="category=Social"
-              >
-                Social
-              </option>
-              <option
-                selected={isFilteringSecond === "category=Sports"}
-                value="category=Sports"
-              >
-                Sports
-              </option>
-              <option
-                selected={isFilteringSecond === "category=Fighting"}
-                value="category=Fighting"
-              >
-                Fighting
-              </option>
+              <option value="category=MMO">MMO</option>
+              <option value="category=MMORPG">MMORPG</option>
+              <option value="category=MOBA">MOBA</option>
+              <option value="category=Shooter">Shooter</option>
+              <option value="category=Strategy">Strategy</option>
+              <option value="category=Racing">Racing</option>
+              <option value="category=Social">Social</option>
+              <option value="category=Sports">Sports</option>
+              <option value="category=Fighting">Fighting</option>
             </select>
           </div>
         </div>
@@ -221,35 +178,16 @@ const Filter = (props) => {
           <div>
             <label htmlFor="category">Sorted By:</label>
             <select
+              value={isFilteringSort}
               onChange={(e) => sortHandler(e.target.value)}
               name="category"
               id="category"
             >
               <option value="">Sort By</option>
-              <option
-                selected={isFilteringSort === "sort-by=relevance"}
-                value="sort-by=relevance"
-              >
-                Relevance
-              </option>
-              <option
-                selected={isFilteringSort === "sort-by=popularity"}
-                value="sort-by=popularity"
-              >
-                Popularity
-              </option>
-              <option
-                selected={isFilteringSort === "sort-by=release-date"}
-                value="sort-by=release-date"
-              >
-                Release Date
-              </option>
-              <option
-                selected={isFilteringSort === "sort-by=alphabetical"}
-                value="sort-by=alphabetical"
-              >
-                Alphabetical
-              </option>
+              <option value="sort-by=relevance">Relevance</option>
+              <option value="sort-by=popularity">Popularity</option>
+              <option value="sort-by=release-date">Release Date</option>
+              <option value="sort-by=alphabetical">Alphabetical</option>
             </select>
           </div>
         </div>
